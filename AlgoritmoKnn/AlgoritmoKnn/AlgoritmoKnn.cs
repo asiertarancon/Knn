00daca0ea08e9aa9a08d2ea121dc9791416ea4d8
Asier <asiertarancon@outlook.com>
2016-01-03 12:29:10 +0100

    Knn y Kmeans completed

---
+++ AlgoritmoKnn/AlgoritmoKnn/AlgoritmoKnn.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmosMachineLearning
+{    
+    class AlgoritmoKnn
+    {
+        /// <summary>
+        /// Conjunto de datos de entrenamiento en el que se basará el algoritmo
+        /// </summary>
+        public ConjuntoDatos ConjuntoDatos { get; set; }
+        /// <summary>
+        /// Número de vecinos más cercanos que comprobará el algoritmo
+        /// </summary>
+        public int K { get; set; }
+        /// <summary>
+        /// Dado una instancia de datos nueva encuentra los k vecinos más cercanos        
+        /// </summary>
+        /// <param name="dato">Dato a clasificar</param>
+        /// <param name="indices">Índices de los datos de entrenamiento que utilizar</param>
+        /// <param name="k">Número de vecinos más cercanos a utilizar</param>
+        /// <returns>Lista de parejas clave-valor con la media y los datos</returns>
+        public List<KeyValuePair<double, Dato>> EncuentraKVecinosMasCercanos(Dato dato, List<int> indices, int k)
+        {
+            var vecinos = new List<KeyValuePair<double, Dato>>();
+            foreach (Dato entrenamiento in ConjuntoDatos.Datos)
+            {
+                if (entrenamiento == dato) continue;
+                double distancia = CalculaDistancia(dato, entrenamiento, indices);
+                vecinos.Add(new KeyValuePair<double, Dato>(distancia, entrenamiento));
+            }
+            return vecinos.OrderBy(n => n.Key).Take(k).ToList();
+        }
+
+        /// <summary>
+        /// Clasifica el nuevo dato en función de los datos de entrenamiento 
+        /// </summary>
+        /// <param name="nuevoAClasificar"></param>
+        /// <returns></returns>
+        public double ClasificaNuevoDato(Dato nuevoAClasificar, List<int> indices)
+        {
+            var dic = EncuentraKVecinosMasCercanos(nuevoAClasificar, indices, K);
+
+            var indiceSalida = ConjuntoDatos.Caracteristicas.IndexOf(ConjuntoDatos.Caracteristicas.First(s => s.Tipo == Tipo.salida));
+
+            var moda = dic.Select(a => a.Value[indiceSalida])
+                          .GroupBy(n => n)
+                          .OrderByDescending(g => g.Count())
+                          .Select(g => g.Key).FirstOrDefault();
+            return moda;
+        }
+
+        /// <summary>
+        /// Calcula la distancia Euclídea dadas dos instancias de datos completas, usando las características según los índices pasados
+        /// </summary>
+        /// <param name="indices">Indices de las características a usar</param>
+        /// <param name="dato">El dato a clasificar</param>
+        /// <param name="entrenamiento">Uno de los datos de entrenamiento</parm>
+        /// <returns>Double</returns>
+        private double CalculaDistancia(Dato dato, Dato entrenamiento, List<int> indices)
+        {
+            double d = 0;
+            foreach (int i in indices)
+            {
+                switch (ConjuntoDatos.Caracteristicas[i].Tipo)
+                {
+                    case Tipo.continuo:
+                        d += Comun.CalculaDistanciaEuclidea(dato[i], entrenamiento[i]);
+                        break;
+                    case Tipo.discreto:
+                        d += (dato[i] == entrenamiento[i]) ? 0 : 1;
+                        break;
+                    case Tipo.salida:
+                    default:
+                        break;
+                }
+            }
+            return Math.Sqrt(d);
+        }        
+    }
+}
--- AlgoritmoKnn/AlgoritmoKnn/Program.cs
+++ AlgoritmoKnn/AlgoritmoKnn/Program.cs
@@ -1,15 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AlgoritmoKnn
+namespace AlgoritmosMachineLearning
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Knn
+            var caracteristicasFlores = new List<Caracteristica>()
+                {
+                    new Caracteristica("sepallength", Tipo.continuo),
+                    new Caracteristica("sepalwidth", Tipo.continuo),
+                    new Caracteristica("petallength", Tipo.continuo),
+                    new Caracteristica("petalwidth", Tipo.continuo),
+                    new Caracteristica("Class", Tipo.salida),
+                };
+
+            KnnTest("./datos/flores.arff", "./datos/resultado_knn_flores.txt", caracteristicasFlores, new Dato(new List<double>() { 4.1, 4.5, 2.4, 1.2 }));
+            KnnTest("./datos/flores.arff", "./datos/resultado_knn_flores.txt", caracteristicasFlores, new Dato(new List<double>() { 4.9, 2.4, 3.3, 1.0 }));
+
+            var caracteristicasGolf = new List<Caracteristica>()
+            {
+                new Caracteristica("Previsión", Tipo.discreto),
+                new Caracteristica("Temperatura", Tipo.continuo),
+                new Caracteristica("Humedad", Tipo.continuo),
+                new Caracteristica("Viento", Tipo.continuo),
+                new Caracteristica("Jugamos", Tipo.salida),
+            };
+
+            KnnTest("./datos/prediccion.arff", "./datos/resultado_knn_prediccion.txt", caracteristicasGolf, new Dato(new List<double>() { 0, 86, 85, 0 }));
+            KnnTest("./datos/prediccion.arff", "./datos/resultado_knn_prediccion.txt", caracteristicasGolf, new Dato(new List<double>() { 2, 68, 80, 0 }));
+
+            //Kmeans
+            KMeansTest("./datos/flores_clustering.arff", "./datos/resultado_kmeans_floresClustering.txt");
+            KMeansTest("./datos/baloncesto.arff", "./datos/resultado_kmeans_baloncesto.txt");
+            Console.ReadLine();
         }
+
+        private static void KMeansTest(string ficheroEntrada, string ficheroSalida)
+        {
+            Comun.MuestraYGuarda("\nAlgoritmo Kmeans. Asier Tarancón. Fichero: " + Path.GetFileName(ficheroSalida), ficheroSalida);
+            Comun.MuestraYGuarda("Hora: " + DateTime.Now, ficheroSalida);
+            var conjuntoDatos = CargaDatosDeFichero(ficheroEntrada);
+
+            Comun.MuestraYGuarda("Datos iniciales:\n", ficheroSalida);
+            Comun.MuestraYGuarda("-------------------", ficheroSalida);
+            AlgoritmoKMeans.MostrarDatos(conjuntoDatos, 1, true, true, texto => Comun.MuestraYGuarda(texto, ficheroSalida));
+
+            int numClusters = 3;
+            Comun.MuestraYGuarda("\nEstablecemos el número de clústers a " + numClusters, ficheroSalida);
+
+            int[] clusters = AlgoritmoKMeans.Clusteriza(conjuntoDatos, numClusters);
+
+            Comun.MuestraYGuarda("\nAgrupamiento Kmeans terminado:\n", ficheroSalida);                       
+            AlgoritmoKMeans.MostrarClusterizado(conjuntoDatos, clusters, numClusters, 1, texto=>Comun.MuestraYGuarda(texto, ficheroSalida));            
+        }
+              
+        private static void KnnTest(string ficheroEntrada, string ficheroSalida, List<Caracteristica> caracteristicas, Dato dato)
+        {           
+            ConjuntoDatos conjuntoDatos = new ConjuntoDatos();
+            conjuntoDatos.Caracteristicas = new List<Caracteristica>(caracteristicas);
+            conjuntoDatos.Datos = CargaDatosDeFichero(ficheroEntrada);
+
+            AlgoritmoKnn knn = new AlgoritmoKnn();
+            knn.ConjuntoDatos = conjuntoDatos;
+            knn.K = 3;
+
+            Comun.MuestraYGuarda("\nAlgoritmo Knn. Asier Tarancón. Fichero: " + Path.GetFileName(ficheroSalida), ficheroSalida);
+            Comun.MuestraYGuarda("Hora: " + DateTime.Now, ficheroSalida);
+            Comun.MuestraYGuarda("Con el vector inicial: " + string.Join(" ", dato.Select(i => i)), ficheroSalida);         
+            Dato nuevoAClasificar = dato;
+            double clasificadoComo = knn.ClasificaNuevoDato(nuevoAClasificar, new List<int>() { 0, 1, 2, 3, 4 });
+
+            Comun.MuestraYGuarda("El elemento nuevo está clasificado como " + clasificadoComo, ficheroSalida);
+        }
+
+        private static List<Dato> CargaDatosDeFichero(string fichero)
+        {
+            try
+            {
+                List<Dato> datosCargados = new List<Dato>();
+                var lineasDatos = File.ReadAllLines(fichero).ToList();
+                while (lineasDatos.FirstOrDefault() != null && lineasDatos.FirstOrDefault().ToLower() != "@data")
+                    lineasDatos.RemoveAt(0);
+                lineasDatos.RemoveAt(0);//Eliminamos la linea @data
+
+                foreach (var linea in lineasDatos)
+                {
+                    try
+                    {
+                        datosCargados.Add(new Dato(linea.Split(',').ToList().Select(d => Convert.ToDouble(d.Replace('.', ','))).ToList()));
+                    }
+                    catch (Exception)
+                    {//Si hay algún problema en la línea que continue a las siguientes
+                    }
+                    
+                }
+                return datosCargados;
+            }
+            catch (Exception ex)
+            {
+                return new List<Dato>();
+            }            
+        }
+
+        
     }
 }
